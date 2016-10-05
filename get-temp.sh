583909f7f7d0524c32cd253184243bec4ad73041
drougier <drougier@student.sierracollege.edu>
2016-10-04 21:09:27 -0700

    Temp logger push

--- get-temp.sh
+++ get-temp.sh
@@ -5,9 +5,9 @@ tc=$(echo "scale=1;(($tc1 / 1000))" |bc)
 tf=$(echo "scale=1;((9/5) * $tc) + 32" |bc)
 
 #Seocnd Part is for the BEER 2 Temperature or AMBIENT
-tc2=$(cat /sys/bus/w1/devices/28-800000035ddb/w1_slave | grep  -E -o ".{0,0}t=.{0,5}" | cut -c 3-)
-tcc=$(echo "scale=1;(($tc1 / 1000))" |bc)
-tff=$(echo "scale=1;((9/5) * $tc) + 32" |bc)
+tc2=$(cat /sys/bus/w1/devices/28-800000013c31/w1_slave | grep  -E -o ".{0,0}t=.{0,5}" | cut -c 3-)
+tcc=$(echo "scale=1;(($tc2 / 1000))" |bc)
+tff=$(echo "scale=1;((9/5) * $tcc) + 32" |bc)
 
 beer1=$tf
 beer2=$tff
--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Current BEER NAME
-beer1name="kwik-e-mart"
-beer2name=""
+beer1name=""
+beer2name="kwik-e-mart"
 
 #Directories
 dir1="/home/pi/fermenter"
@@ -14,18 +14,24 @@ tc1=$(cat /sys/bus/w1/devices/28-800000035ddb/w1_slave | grep  -E -o ".{0,0}t=.{
 tc=$(echo "scale=1;(($tc1 / 1000))" |bc)
 tf=$(echo "scale=1;((9/5) * $tc) + 32" |bc)
 
-#Second Part is for the BEER 2 Temperature or AMBIENT
+#Seocnd Part is for the BEER 2 Temperature or AMBIENT
 tc2=$(cat /sys/bus/w1/devices/28-800000013c31/w1_slave | grep  -E -o ".{0,0}t=.{0,5}" | cut -c 3-)
-tcc=$(echo "scale=1;(($tc1 / 1000))" |bc)
-tff=$(echo "scale=1;((9/5) * $tc) + 32" |bc)
+tcc=$(echo "scale=1;(($tc2 / 1000))" |bc)
+tff=$(echo "scale=1;((9/5) * $tcc) + 32" |bc)
 
 beer1=$tf
 beer2=$tff
+
+#echo $today
+#echo "Beer 1 Temp is: $beer1"
+#echo "Beer 2 Temp is: $beer2"
 
 if [ -z "$beer1name" ]
 then
 	:
 else
+	#mkdir -p /home/pi/beer/$beer1name
+	#dir1="/home/pi/fermenter"
 	echo "$today,$beer1" >> $dir1/$beer1name.csv
 	grep -q "DATE,TEMPERATURE" $dir1/$beer1name.csv || sed -i '1s/^/DATE,TEMPERATURE\n/' $dir1/$beer1name.csv
 fi
@@ -34,7 +40,9 @@ if [ -z "$beer2name" ]
 then
         :
 else
-	echo "$today,$beer2" >> $dir1/$beer2name.csv
+	#mkdir -p /home/pi/beer/$beer2name
+        #dir2="/home/pi/fermenter"
+        echo "$today,$beer2" >> $dir1/$beer2name.csv
 	grep -q "DATE,TEMPERATURE" $dir1/$beer2name.csv || sed -i '1s/^/DATE,TEMPERATURE\n/' $dir1/$beer2name.csv
 fi
 
